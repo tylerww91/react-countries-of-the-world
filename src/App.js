869156dc8cfb2dc815720250f1d91e250fcc1461
Tyler Watson <tylerww91@gmail.com>
2023-01-18 15:40:37 -0800

    builds out Footer component and mounts it in app.js, adds various css styling to Header.css Footer.css and Main.css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 
 import Header from './components/layout/Header/Header';
 import Main from './components/Main/Main';
-// import Footer from './components/layout/Footer/Footer';
+import Footer from './components/layout/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
